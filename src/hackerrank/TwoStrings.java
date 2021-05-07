e564643da6bd64a66548f120207b36d455910b09
Abdulrahman.Rashad <Abdulrahman.Rashad@warbabank.com>
2021-05-07 16:21:05 +0200

    Initial commit

---
+++ src/hackerrank/TwoStrings.java
@@ -0,0 +1,25 @@
+package hackerrank;//problem link on hacker rank
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoStrings {
+    public static String twoStrings(String s1, String s2) {
+        Set<Character> characterSet = new HashSet<>();
+        for (int i = 0; i < s1.length(); i++) {
+            characterSet.add(s1.charAt(i));
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            boolean contains = characterSet.contains(s2.charAt(i));
+            if (contains) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(twoStrings("wouldyoulikefries", "abcabcabcabcabcabc"));
+
+    }
+}
