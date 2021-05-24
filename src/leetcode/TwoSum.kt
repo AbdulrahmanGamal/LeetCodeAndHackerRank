5f240f1d5831e30d71b2da85d4fc7956795bd247
Abdulrahman.Rashad <Abdulrahman.Rashad@warbabank.com>
2021-05-24 23:28:58 +0200

    - two sum

---
+++ src/leetcode/TwoSum.kt
@@ -0,0 +1,25 @@
+package leetcode
+
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    var sums = hashMapOf<Int, Int>()
+    val arr = IntArray(2)
+    nums.forEachIndexed { index, element ->
+        val pair = target - element
+        if (sums.containsKey(pair)) {
+
+            arr[0]= index
+            arr[1]= sums.get(pair)!!
+        } else {
+            sums[element] = index
+        }
+    }
+    return arr
+}
+
+fun main() {
+    val arr= intArrayOf( 2,7,11,15)
+    twoSum(arr,9).forEach {
+        println(it)
+    }
+}
